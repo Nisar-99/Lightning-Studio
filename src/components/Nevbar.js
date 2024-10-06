218ebb187320761892f3b5c512b51f66094ab19d
NisarAhmad99 <nahmad@northteq.com>
2024-10-06 12:04:06 +0530

    first

--- src/components/Nevbar.js
+++ src/components/Nevbar.js
@@ -1,12 +1,12 @@
 
 export default function initialize() {
 
-    
+
 
     return (
         <div className="nav-scroller py-1 border-bottom">
             <nav className="nav nav-underline justify-content-evenly">
-                <a className="nav-item nav-link link-body-emphasis" href="/">Home</a>
+                <a className="nav-item nav-link link-body-emphasis" href="https://nisar-99.github.io/Lightning-Studio">Home</a>
                 <a className="nav-item nav-link link-body-emphasis" href="#/release-notes">Release Notes</a>
                 <a className="nav-item nav-link link-body-emphasis" href="#/privacy-policy">Privacy Policy</a>
                 <a className="nav-item nav-link link-body-emphasis"
